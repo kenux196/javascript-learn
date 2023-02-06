d0d4f26fba181db7cc68af6ddba654304075eba8
skyun <skyun@a2tec.co.kr>
2023-02-06 15:39:34 +0900

    modify fetch error

--- pure-javascript/js-fetch.js
+++ pure-javascript/js-fetch.js
@@ -1,7 +1,10 @@
-let url = 'https://kenux.duckdns.org'
+import fetch from "node-fetch";
+
+let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits'
 let response = await fetch(url);
 if (response.ok) {
   let json = await response.json();
+  console.log(json);
 } else {
   console.error("HTTP-Error: " + response.status);
 }
