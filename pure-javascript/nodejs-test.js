2656e88e300cd8122f058b3f7d261cf728a082f7
skyun <skyun@a2tec.co.kr>
2023-02-06 15:44:35 +0900

    remove file

--- pure-javascript/nodejs-test.js
+++
@@ -1,7 +0,0 @@
-let http = require('http');
-
-http.createServer(fucntion(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain'});
-  response.end('Hello world')
-}).listen(9090);
-console.log("Server running at http://127.0.0.1:9090");
