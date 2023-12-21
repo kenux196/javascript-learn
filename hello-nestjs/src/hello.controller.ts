894701052a6735855086986cda89330d1888ca2d
kenux196 <kenux.yun@gmail.com>
2023-12-21 14:54:23 +0900

    Modify hello-nestjs

--- hello-nestjs/src/hello.controller.ts
+++ hello-nestjs/src/hello.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller()
 export class HelloController {
   @Get()
   hello() {
+    console.log('Request GET /');
     return '안녕하세요! NestJS로 만든 첫 애플리케이션입니다.';
+  }
+
+  @Get('/me')
+  getName(@Query('name') name: String) {
+    console.log(`Request GET /me params: ${name}`);
+    return `당신은 ${name} 입니다.`;
   }
 }
--- hello-nestjs/src/main.ts
+++ hello-nestjs/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { HelloModule } from './hello.module';
 
 async function bootstrap() {
+  console.log('Create app by NestFactory');
   const app = await NestFactory.create(HelloModule);
+  console.log('Created app');
+
   await app.listen(3000, () => {
     console.log('Start server...!');
   });
