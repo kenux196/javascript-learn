f8d8f43f4c830bb277f5b5c89681cbefb232807e
kenux196 <kenux.yun@gmail.com>
2023-12-24 14:23:04 +0900

    Modify app.module : set up TypeOrmModule

--- nest-auth-test/src/app.module.ts
+++ nest-auth-test/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'nest-auth-test.sqlit',
+      entities: [],
+      synchronize: true, // 개발에서만 사용할 것. 자동으로 db 스키마 생성하는 기능임
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
