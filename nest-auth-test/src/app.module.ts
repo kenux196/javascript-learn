d00f3155ba35b716d6e836a2fdda8416587b35f3
kenux196 <kenux.yun@gmail.com>
2023-12-24 15:08:18 +0900

    add crud for user entity

--- nest-auth-test/src/app.module.ts
+++ nest-auth-test/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'nest-auth-test.sqlit',
-      entities: [],
+      entities: [User],
       synchronize: true, // 개발에서만 사용할 것. 자동으로 db 스키마 생성하는 기능임
       logging: true,
     }),
--- nest-auth-test/src/user/user.service.ts
+++ nest-auth-test/src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(user): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string) {
+    console.log(`getUser(${email})`);
+    const result = await this.userRepository.findOne({
+      where: { email: email },
+    });
+    console.log(`result: ${result}`);
+    return result;
+  }
+
+  async updateUser(email, _user) {
+    // const user: User = await this.getUser(email);
+    const user: User = await this.getUser(email);
+    console.log(_user);
+    user.username = _user.username;
+    user.password = _user.password;
+    console.log(user);
+    this.userRepository.save(user);
+  }
+
+  deleteUser(email) {
+    return this.userRepository.delete({ email: email });
+  }
+}
