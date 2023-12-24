c9d03486d9feb610711c62de60389d5ba3d63d4b
kenux196 <kenux.yun@gmail.com>
2023-12-24 21:26:52 +0900

    Add auth register

--- nest-auth-test/src/app.module.ts
+++ nest-auth-test/src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'nest-auth-test.sqlit',
+      database: 'nest-auth-test.sqlite',
       entities: [User],
       synchronize: true, // 개발에서만 사용할 것. 자동으로 db 스키마 생성하는 기능임
       logging: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
