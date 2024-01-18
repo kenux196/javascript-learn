e6751f17d8d995f8aed4e865ca5f4246118f10b3
kenux196 <kenux.yun@gmail.com>
2024-01-18 13:21:44 +0900

    add feature for signup

--- nest-board-app/src/auth/auth.service.ts
+++ nest-board-app/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from 'src/users/users.repository';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await this.usersRepository.save(user);
+  }
+}
