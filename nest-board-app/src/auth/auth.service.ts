52548694d01b71af2fd7a6311c16103b27b1c1f3
kenux196 <kenux.yun@gmail.com>
2024-01-18 13:45:44 +0900

    apply encrypted password

--- nest-board-app/src/auth/auth.service.ts
+++ nest-board-app/src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import {
 import { UsersRepository } from 'src/users/users.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from 'src/users/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -13,9 +14,16 @@ export class AuthService {
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
+
+    const salt = await bcrypt.genSalt();
+    console.log('salt = ', salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log('hashedPassword = ', hashedPassword);
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.password = hashedPassword;
+
     try {
       await this.usersRepository.save(user);
     } catch (error) {
